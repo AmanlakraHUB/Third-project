ecb2b5f2b60968e7511c28086f06a1659d679e2b
Aman Lakra <amanlakra1111@gmail.com>
2023-01-06 00:21:15 +0530

    initial commit

---
+++ Ninth-page/src/pages/Home.js
@@ -0,0 +1,277 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.homeDiv}>
+      <div className={styles.frameDiv}>
+        <div className={styles.iconText}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Internship Description</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Internship Guide</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Surveys</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Settings</div>
+        </div>
+      </div>
+      <div className={styles.frameDiv1}>
+        <div className={styles.frameDiv2}>
+          <div className={styles.iconText}>
+            <img
+              className={styles.arrowLeftIcon}
+              alt=""
+              src="../arrowleft.svg"
+            />
+            <div className={styles.textDiv}>Back</div>
+          </div>
+        </div>
+        <div className={styles.frameDiv3}>
+          <div className={styles.textDiv5}>Add New Internship</div>
+        </div>
+        <div className={styles.frameDiv4}>
+          <div className={styles.frameDiv5}>
+            <div className={styles.continueToNextStep}>
+              Continue to Next Step
+            </div>
+            <img
+              className={styles.arrowRightIcon}
+              alt=""
+              src="../arrowright.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameDiv6}>
+        <div className={styles.frameDiv7}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv9}>
+              <div className={styles.categoryDiv}>Category</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright1.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv10}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Description</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv13}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Location</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv16}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Benefits</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright4.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv19}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Intro Video</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright4.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv22}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Mentor Details</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv25}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv6}>
+            <div className={styles.frameDiv9}>
+              <div className={styles.categoryDiv}>Recommended Roles</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle6.svg"
+              />
+            </div>
+            <img
+              className={styles.untickCircleIcon}
+              alt=""
+              src="../arrowright7.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv28}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div
+                className={styles.categoryDiv}
+              >{`Web Links & Resources`}</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle6.svg"
+              />
+            </div>
+            <img
+              className={styles.untickCircleIcon}
+              alt=""
+              src="../arrowright1.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.assessmentDiv}>
+          <div className={styles.frameDiv31}>
+            <img
+              className={styles.arrowRightIcon}
+              alt=""
+              src="../addsquare.svg"
+            />
+            <div className={styles.addMoreDiv}>Add More</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.contentDiv}>
+        <div className={styles.categoryDiv}>Recommended Roles</div>
+        <div className={styles.frameDiv32}>
+          <div className={styles.textFieldDiv}>
+            <div className={styles.textDiv6}>Search Roles</div>
+            <img
+              className={styles.arrowRightIcon}
+              alt=""
+              src="../searchnormal.svg"
+            />
+            <img
+              className={styles.icons16pxarrowDownFilled}
+              alt=""
+              src="../icons16pxarrowdownfilled.svg"
+            />
+          </div>
+          <div className={styles.frameDiv33}>
+            <div className={styles.professionPillsDiv}>
+              <div className={styles.fullstackDeveloperDiv}>
+                Fullstack Developer
+              </div>
+              <img className={styles.closeIcon} alt="" src="../close.svg" />
+            </div>
+            <div className={styles.professionPillsDiv}>
+              <div className={styles.fullstackDeveloperDiv}>
+                Backend Developer
+              </div>
+              <img className={styles.closeIcon} alt="" src="../close.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ Seventh-page/src/pages/Home.js
@@ -0,0 +1,276 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.homeDiv}>
+      <div className={styles.frameDiv}>
+        <div className={styles.iconText}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Internship Description</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Internship Guide</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Surveys</div>
+        </div>
+        <div className={styles.iconText1}>
+          <img
+            className={styles.untickCircleIcon}
+            alt=""
+            src="../untickcircle.svg"
+          />
+          <div className={styles.textDiv}>Settings</div>
+        </div>
+      </div>
+      <div className={styles.frameDiv1}>
+        <div className={styles.frameDiv2}>
+          <div className={styles.iconText}>
+            <img
+              className={styles.arrowLeftIcon}
+              alt=""
+              src="../arrowleft.svg"
+            />
+            <div className={styles.textDiv}>Back</div>
+          </div>
+        </div>
+        <div className={styles.frameDiv3}>
+          <div className={styles.textDiv5}>Add New Internship</div>
+        </div>
+        <div className={styles.frameDiv4}>
+          <div className={styles.frameDiv5}>
+            <div className={styles.continueToNextStep}>
+              Continue to Next Step
+            </div>
+            <img
+              className={styles.arrowRightIcon}
+              alt=""
+              src="../arrowright.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameDiv6}>
+        <div className={styles.frameDiv7}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv9}>
+              <div className={styles.categoryDiv}>Category</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright1.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv10}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Description</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv13}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Location</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv16}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Benefits</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright4.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv19}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Intro Video</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright4.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv22}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv5}>
+            <div className={styles.frameDiv9}>
+              <div className={styles.categoryDiv}>Mentor Details</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle5.svg"
+              />
+            </div>
+            <img
+              className={styles.untickCircleIcon}
+              alt=""
+              src="../arrowright6.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv25}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div className={styles.categoryDiv}>Recommended Roles</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle5.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright2.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.frameDiv28}>
+          <div className={styles.frameDiv8}>
+            <img className={styles.untickCircleIcon} alt="" src="../menu.svg" />
+          </div>
+          <div className={styles.moduleDiv}>
+            <div className={styles.frameDiv12}>
+              <div
+                className={styles.categoryDiv}
+              >{`Web Links & Resources`}</div>
+              <img
+                className={styles.untickCircleIcon}
+                alt=""
+                src="../tickcircle5.svg"
+              />
+            </div>
+            <img
+              className={styles.arrowRightIcon1}
+              alt=""
+              src="../arrowright1.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.assessmentDiv}>
+          <div className={styles.frameDiv31}>
+            <img
+              className={styles.arrowRightIcon}
+              alt=""
+              src="../addsquare.svg"
+            />
+            <div className={styles.addMoreDiv}>Add More</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.contentDiv}>
+        <div className={styles.mentorDetailsDiv1}>Mentor Details</div>
+        <div className={styles.rectangleDiv} />
+        <div className={styles.frameDiv32}>
+          <div className={styles.frameDiv33}>
+            <img
+              className={styles.untickCircleIcon}
+              alt=""
+              src="../profile.svg"
+            />
+            <div className={styles.frameDiv34}>
+              <div className={styles.textDiv6}>Name</div>
+            </div>
+          </div>
+          <div className={styles.frameDiv33}>
+            <img className={styles.untickCircleIcon} alt="" src="../sms.svg" />
+            <div className={styles.frameDiv34}>
+              <div className={styles.textDiv6}>Email address</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameDiv37}>
+          <img className={styles.untickCircleIcon} alt="" src="../link.svg" />
+          <div className={styles.frameDiv34}>
+            <div className={styles.textDiv6}>LinkedIn URL (optional)</div>
+          </div>
+        </div>
+        <div className={styles.frameDiv39}>
+          <img className={styles.imageIcon} alt="" src="../image.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
